cd1a002d05e507640f009ba6484fff71dc202b00
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-03-30 11:10:31 +0300

    Update request_sendimage.py

--- request_sendimage.py
+++ request_sendimage.py
@@ -13,3 +13,32 @@ parameters = {
 
 resp = requests.get(base_url, data = parameters)
 print(resp.text)
+
+# Send multiple images to [chat_id] using [bot_id]
+
+import requests
+import time
+
+base_url = "https://api.telegram.org/bot[bot_id]/sendPhoto"
+
+# links to images
+images = ["https1",
+          "https2"]
+
+descriptions = ['image1','image2']
+
+# image send interval
+intervals = 0.1
+
+ii=-1
+for image in images:
+    ii+=1
+    time.sleep(intervals)  
+    parameters = {
+      "chat_id" : "[chat_id]",
+      "photo" : url,
+      "caption" : descriptions[ii]}
+
+  resp = requests.get(base_url,
+                      data = parameters)
+  print(resp.text)
