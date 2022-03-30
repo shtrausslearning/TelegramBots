3a9cf0312e9faeb16a9021982f73d5bd0e0ba8d4
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-03-30 13:39:43 +0300

    Create request_setpoll_getresults.py

---
+++ request_setpoll_getresults.py
@@ -0,0 +1,49 @@
+# Create a Poll in group [chat_id] using [bot_id] & extract and plot results
+
+import requests
+import json
+
+base_url = "https://api.telegram.org/bot[bot_id]/sendPoll"
+
+parameters = {
+    "chat_id" : "[chat_id]",
+    "question" : "How much is 3+3?",
+    "options" : json.dumps(['3','6','9']),
+    "is_anonymous" : True,
+    "allows_multiple_answers" : True,
+    "type" : "regular",
+    "explanation" : "Test Poll"
+}
+
+resp = requests.get(base_url,data=parameters)
+resp.json() # show request content
+
+# Get Show all Updates for Bot Interactions
+# Eg. if bot is located in group, updates will contain group(chat_id) data
+base_url = "https://api.telegram.org/bot[bot_id]/getUpdates"
+
+parameters = {
+    "offset" : "offset_id"
+}
+
+resp = requests.get(base_url,
+                    data=parameters)
+# list of all updates
+lst_updIDs = resp.json()['result'] 
+
+# show poll results
+import plotly.express as px
+
+# Select the desired poll update
+options = lst_updIDs[-4]['poll']['options']
+
+# Store Data in lists
+lst_res = []; lst_options = []
+for i in options:
+    option = i['text']
+    votes = i['voter_count']
+    lst_res.append(votes)
+    lst_options.append(option)
+   
+# Plot results
+px.bar(x=lst_options,y=lst_res)
