34c23bb9c99c37840f60ef592f043331d2a5cb25
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-03-30 10:25:43 +0300

    Create request_quizpoll.py

---
+++ request_quizpoll.py
@@ -0,0 +1,20 @@
+# Using request library & url link to send a quiz
+# to [chat_id] using created bot[X] identifier
+
+import requests
+import json
+
+base_url = "https://api.telegram.org/bot[X]/sendPoll"
+
+parameters = {
+    "chat_id" : "[chat_id]",
+    "question" : "How much is 3+3?",
+    "options" : json.dumps(['3','6','9']),
+    "is_anonymous" : True,
+    "type" : "quiz",
+    "correct_option_id": 2,
+    "explanation" : "Test Quiz"
+}
+
+resp = requests.get(base_url,data=parameters)
+resp.json()
